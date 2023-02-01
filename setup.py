9932886cf047200bc53ee28dca8320f218031e42
nicholsons <sarah.sanders@ucl.ac.uk>
2023-02-01 15:27:17 +0000

    added dash app

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="comp0034-week10",
-    packages=["paralympic_app", "iris_app"],
+    packages=["paralympic_app", "iris_app", "paralympic_dash_app"],
     include_package_data=True,
     install_requires=[
         "flask",
